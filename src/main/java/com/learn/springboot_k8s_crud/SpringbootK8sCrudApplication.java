af3e630d537c63c482fc40bb94543451b867ff9b
dhanushkasampath <dhanushka.2018410@iit.ac.lk>
2024-07-22 22:29:43 +0530

    first

---
+++ src/main/java/com/learn/springboot_k8s_crud/SpringbootK8sCrudApplication.java
@@ -0,0 +1,13 @@
+package com.learn.springboot_k8s_crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootK8sCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootK8sCrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/learn/springboot_k8s_crud/SpringbootK8sCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn.springboot_k8s_crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootK8sCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
